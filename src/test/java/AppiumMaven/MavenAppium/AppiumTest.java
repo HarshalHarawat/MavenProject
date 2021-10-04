5c4fe11ada93c8599415a2ee6c34e85595d4507b
Harshal Harawat <harshaljain830>
2021-10-04 18:05:36 +0530

    MycodeforMaven

---
+++ src/test/java/AppiumMaven/MavenAppium/AppiumTest.java
@@ -0,0 +1,14 @@
+package AppiumMaven.MavenAppium;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest
+{
+	@Test
+	void App()
+	{
+		System.out.println("Appium for mobile test");
+	}
+	
+
+}
---
+++ src/test/java/AppiumMaven/MavenAppium/SeleniumTest.java
@@ -0,0 +1,13 @@
+package AppiumMaven.MavenAppium;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest 
+{
+	@Test
+	void sell()
+	{
+		System.out.println("ASelenium for Web Browser test");
+	}
+
+}
